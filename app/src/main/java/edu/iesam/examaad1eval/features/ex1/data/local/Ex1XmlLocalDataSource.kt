cbba0a4899006b88252c22ff87bdb3c3dd076ee3
alfsuace <alfonso.suaace@educa.jcyl.es>
2024-11-27 18:23:20 +0100

    shared

--- app/src/main/java/edu/iesam/examaad1eval/features/ex1/data/local/Ex1XmlLocalDataSource.kt
+++ app/src/main/java/edu/iesam/examaad1eval/features/ex1/data/local/Ex1XmlLocalDataSource.kt
@@ -10,66 +10,61 @@ import edu.iesam.examaad1eval.features.ex1.domain.User
 
 class Ex1XmlLocalDataSource(private val context: Context) {
 
+
+
     private val gson = Gson()
 
     private val sharedPref = context.getSharedPreferences(
         context.getString(R.string.ex1), Context.MODE_PRIVATE
     )
 
-    fun getUsers(): List<User> {
-        val users = mutableListOf<User>()
-        sharedPref.all.values.forEach {
-            when(gson.fromJson(it.toString(), User::class.java)){
-                is User -> users.add(gson.fromJson(it as String, User::class.java))
-            }
-        }
-        return users
-    }
 
     fun saveUsers(users: List<User>) {
         sharedPref.edit {
-            users.forEach {
-                putString("usersId ${it.id}", gson.toJson(it))
+                putString("users", gson.toJson(users))
                 apply()
-            }
         }
     }
+
+    fun getUsers(): List<User> {
+        val json = sharedPref.getString("users", null)
+        return if (json != null) {
+            gson.fromJson(json, Array<User>::class.java).toList()
+        } else {
+            emptyList()
+        }
+    }
+
+   fun saveItems(items: List<Item>) {
+        sharedPref.edit {
+            putString("items", gson.toJson(items))
+            apply()
+        }
+   }
 
     fun getItems(): List<Item> {
-        val items = mutableListOf<Item>()
-        sharedPref.all.values.forEach {
-            when(gson.fromJson(it as String, Item::class.java)){
-                is Item -> items.add(gson.fromJson(it, Item::class.java))
-            }
+        val json = sharedPref.getString("items", null)
+        return if (json != null) {
+            gson.fromJson(json, Array<Item>::class.java).toList()
+        } else {
+            emptyList()
         }
-        return items
-    }
-
-    fun saveItems(items: List<Item>) {
-        sharedPref.edit {
-            items.forEach {
-                putString("itemsId ${it.id}", gson.toJson(items))
-                apply()
-            }
-        }
-    }
-
-    fun getServices(): List<Services> {
-        val services = mutableListOf<Services>()
-        sharedPref.all.values.forEach {
-            when(gson.fromJson(it as String , Services::class.java)){
-                is Services -> services.add(gson.fromJson(it, Services::class.java))
-            }
-        }
-        return services
     }
 
     fun saveServices(services: List<Services>) {
         sharedPref.edit {
-            services.forEach {
-                putString("servicesId ${it.id}", gson.toJson(it))
-                apply()
-            }
+            putString("services", gson.toJson(services))
+            apply()
         }
     }
+
+    fun getServices(): List<Services> {
+        val json = sharedPref.getString("services", null)
+        return if (json != null) {
+            gson.fromJson(json, Array<Services>::class.java).toList()
+        } else {
+            emptyList()
+        }
+    }
+
 }
